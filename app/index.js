9076018e51656c9f66204e9019743a56c36605f4
Foyzul Karim <foyzulkarim@gmail.com>
2020-08-21 00:45:23 +0800

    User saved OK (#1)

--- app/index.js
+++ app/index.js
@@ -1,22 +1,48 @@
 import express from "express";
+import models from "./models/index.js";
+import mongoose from "mongoose";
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {    
-    res.send('hello viewers '+ req.query.id);
+const log = (msg) => console.log(msg);
+
+const uri = "mongodb://localhost:27017/parcelkoi";
+const options = {};
+
+const connectWithDb = () => {
+    mongoose.connect(uri, options, (err, db) => {
+        if (err) {
+            console.error(err);
+        }
+
+        else log("database connection established");
+    });
+};
+
+connectWithDb();
+
+app.get('/', (req, res) => {
+    res.send('hello viewers ' + req.query.id);
 });
 
 app.post('/', (req, res) => {
     const body = req.body;
-    res.send('hello viewers this is post '+ body.message);
+    const user = new models.User({ username: body.username, createdAt: new Date() });
+    user.save().then((savedUser) => {
+        res.status(201).send('User saved. Id: ' + savedUser._id);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
 })
 
 app.listen(port, () => {
     console.log("Listening to port " + port);
 });
+
+log(models);
 
 /*
 1. up and running the express server
