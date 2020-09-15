922bd942b6f58a8d2eed5f61ed78493b46acda9d
Foyzul Karim <foyzulkarim@gmail.com>
2020-09-16 00:23:08 +0800

    refactored. done

--- app/index.js
+++ app/index.js
@@ -6,7 +6,6 @@ import winston from "winston";
 import expressWinston from "express-winston";
 import winstonDaily from "winston-daily-rotate-file";
 import winstonMongo from "winston-mongodb";
-//const ElasticsearchTransport = require('winston-elasticsearch');
 import { ElasticsearchTransport } from "winston-elasticsearch";
 
 const port = 3000;
@@ -14,20 +13,23 @@ const app = express();
 
 app.use(express.json());
 
-const processRequest = async (err, req, res, next) => {
-    req.timestamp = new Date();
-    req.correlationId = req.headers['x-correlation-id'];
-    if (!req.correlationId) {
-        req.correlationId = Date.now().toString()
+const processRequest = async (req, res, next) => {
+    var correlationId = req.headers['x-correlation-id'];
+    if (!correlationId) {
+        req.headers['x-correlation-id'] = Date.now().toString();
+        correlationId = req.headers['x-correlation-id'];
     }
-    return next(err, req, res, next);
+
+    res.set('x-correlation-id', correlationId);
+
+    return next();
 }
 
-//app.use(processRequest);
+app.use(processRequest);
 
 connectWithDb();
 
-var infoTransport = new (winston.transports.DailyRotateFile)({
+var fileTransport = new (winston.transports.DailyRotateFile)({
     filename: 'application-info-%DATE%.log',
     datePattern: 'YYYY-MM-DD-HH',
     zippedArchive: true,
@@ -43,37 +45,7 @@ const esTransportOpts = {
 
 const esTransport = new (ElasticsearchTransport)(esTransportOpts);
 
-var getMessages = function (req, res) {
-    var msgObj = {
-        correlationId: req.headers['x-correlation-id'].toString(),
-        requestBody: JSON.stringify(req.body)
-    };
-
-    return JSON.stringify(msgObj);
-}
-
-const logger = expressWinston.logger({
-    transports: [
-        new winston.transports.Console(),
-        infoTransport,
-        esTransport
-    ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.json()
-    ),
-    meta: false,
-    msg: getMessages
-    //msg: "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-    //msg: "{ 'correlationId':'{{req.headers['x-correlation-id']}}', 'error' : '{{err.message}}', 'request': '{{JSON.stringify(req.body)}}', 'timestamp': '{{req.timestamp}}'}",
-
-});
-
-app.use(logger);
-
-configure(app);
-
-var errorTransport = new (winston.transports.DailyRotateFile)({
+var fileErrorTransport = new (winston.transports.DailyRotateFile)({
     filename: 'application-error-%DATE%.log',
     datePattern: 'YYYY-MM-DD-HH',
     zippedArchive: true,
@@ -81,26 +53,55 @@ var errorTransport = new (winston.transports.DailyRotateFile)({
     maxFiles: '14d'
 });
 
-var errorMongoTransport = new winston.transports.MongoDB({
+var mongoErrorTransport = new winston.transports.MongoDB({
     db: uri,
     metaKey: 'meta',
+});
+
+var getLogMessage = function (req, res) {
+    var msgObj = {
+        correlationId: req.headers['x-correlation-id'],
+        requestBody: req.body
+    };
+
+    return JSON.stringify(msgObj);
+}
+
+const infoLogger = expressWinston.logger({
+    transports: [
+        new winston.transports.Console(),
+        fileTransport,
+        esTransport
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+    ),
+    meta: false,
+    msg: getLogMessage
 });
 
 const errorLogger = expressWinston.errorLogger(
     {
         transports: [
             new winston.transports.Console(),
-            errorTransport,
-            errorMongoTransport
+            fileErrorTransport,
+            mongoErrorTransport,
+            esTransport
         ],
         format: winston.format.combine(
             winston.format.colorize(),
             winston.format.json()
         ),
         meta: true,
-        msg: "{ 'correlationId':'{{req.headers['x-correlation-id']}}', 'error' : '{{err.message}}', 'request': '{{JSON.stringify(req.body)}}', 'timestamp': '{{req.timestamp}}'}",
+        msg: '{ "correlationId": "{{req.headers["x-correlation-id"]}}", "error" : "{{err.message}}" }',
         correlationId: "{{req.headers['x-correlation-id']}}"
     });
+
+
+app.use(infoLogger);
+
+configure(app);
 
 app.use(errorLogger);
 
@@ -110,19 +111,3 @@ app.listen(port, () => {
     console.log("Listening to port " + port);
     console.log("express-winston demo listening on port %d in %s mode", port, app.settings.env);
 });
-
-/*
-1. up and running the express server
-2. configure the express server
-3. handle the routes of the server
-
-- use directory import
-- use async awaiimport { handleErrors } from './middlewares/handleErrors';
-t function
-
-- 3 layer architecture
-    UserController = controller layer : process the http requests
-    UserService = service layer : process the object and send to data layer
-    Mongoose wrapper = data layer : process the data and get/set it to database
-
-*/
--- app/middlewares/handleErrors.js
+++ app/middlewares/handleErrors.js
@@ -5,17 +5,9 @@ export const handleErrors = async (err, req, res, next) => {
     let code = 500;
     if (err instanceof GeneralError) {
         code = err.getCode();
-        // { name: err.name, message: err.message }
     }
 
     let correlationId = req.headers['x-correlation-id'];
-    if (!correlationId) {
-        correlationId = Date.now().toString();
-    }
-
-    res.set('x-correlation-id', correlationId);
-    
     let msg = { 'correlationId': correlationId, 'error': err.message };
-    // we don't know any known error if we come into this point     
     return res.status(code).json(msg);
 }
