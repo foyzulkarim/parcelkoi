b1277b05210fd562d4b1f89fa865659150b3bda8
Foyzul Karim <foyzulkarim@gmail.com>
2020-09-17 12:49:25 +0800

    Added different types of logging (#6)

--- app/middlewares/handleErrors.js
+++ app/middlewares/handleErrors.js
@@ -2,13 +2,13 @@
 import { GeneralError } from './../utils/errors';
 
 export const handleErrors = async (err, req, res, next) => {
+    let code = 500;
     if (err instanceof GeneralError) {
-        const code = err.getCode();
-        return res.status(code).json({ name: err.name, message: err.message });
+        code = err.getCode();        
     }
-
-    // we don't know any known error if we come into this point 
-    return res.status(500).json({
-        name: 'Internal Server Error', message: err.message
+    
+    let correlationId = req.headers['x-correlation-id'];
+    return res.status(code).json({
+        correlationId: correlationId , message: err.message
     });
 }
