dc79569dca80249b1203ca98f9ca05de98b57c12
Foyzul Karim <foyzulkarim@gmail.com>
2020-09-22 23:01:06 +0800

    WIP

--- app/app.test.js
+++ app/app.test.js
@@ -1,33 +1,73 @@
 import request from 'supertest';
 import app from './app';
+// import userService from './services/userService';
+import models from './models';
+import mongoose from "mongoose";
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-beforeAll(async () => {
-    console.log('before all');
-});
+let mongoServer;
+const mongod = new MongoMemoryServer();
+const userData = { username: 'TekLoon', createdAt: new Date() };
 
-afterAll(async () => {
-    console.log('after all');
-});
-
-beforeEach(async () => {
-    console.log('before each');
-});
-
-afterEach(async () => {
-    console.log('after each');
-})
-
+const mongoUri = 'mongodb://localhost:27017/parcelkoi';
 
 describe('user controller test suite', () => {
-    test('should work', async () => {
-        console.log('my first test');
+
+    // let connection;
+    // let db;
+
+    beforeAll(async () => {
+        console.log('before all');
+        mongoServer = new MongoMemoryServer();
+        const mongoUri = await mongoServer.getUri();
+        await mongoose.connect(mongoUri, opts, (err) => {
+            if (err) console.error(err);
+        });
+    });
+
+    afterAll(async () => {
+        console.log('after all');
+        await mongoose.disconnect();
+        await mongoServer.stop();
+    });
+
+    beforeEach(async () => {
+        console.log('before each');
+    });
+
+    afterEach(async () => {
+        console.log('after each');
     })
+
+    // test('should work', () => {
+    //     console.log('my first test');
+    // });
+
+
+    test('create & save user successfully', async () => {
+
+        const validUser = new models.User(userData);
+        const savedUser = await validUser.save();
+        console.log(savedUser);
+        //Object Id should be defined when successfully saved to MongoDB.
+        expect(savedUser._id).toBeDefined();
+        expect(savedUser.name).toBe(userData.name);
+        expect(savedUser.gender).toBe(userData.gender);
+        expect(savedUser.dob).toBe(userData.dob);
+        expect(savedUser.loginUsing).toBe(userData.loginUsing);
+    });
 
     test('get all users should return list of users', async () => {
         console.log('get all users test');
         let response = await request(app).get('/users');
-        expect(response.body).not.toBeNull();
-        console.log(response.body);
-    })
+        expect(response.statusCode).toBe(200);
+    });
+
+    // test('add user should return user id', async () => {
+    //     const users = db.collection('users');
+    //     const mockUser = { _id: 'some-user-id', username: 'John' };
+    //     let response = await request(app).post('/users').send({ 'username': 'john' });
+    //     expect(response.statusCode).toBe(201);
+    // })
 })
 
