bbf3acd5ac59108647fc36dfac88c6c40bde0b2e
Foyzul Karim <foyzulkarim@gmail.com>
2020-10-18 00:53:37 +0800

    Added swagger.

--- app/src/app.js
+++ app/src/app.js
@@ -3,6 +3,7 @@ import configureRoutes from "./controllers";
 import { handleRequest, handleError } from "./middlewares/index";
 import { infoLogger } from "./logger";
 import dotenv from "dotenv";
+const swaggerUI = require("swagger-ui-express");
 
 dotenv.config();
 
@@ -18,5 +19,8 @@ if (process.env.ENVIRONMENT != 'TEST')
 configureRoutes(app);
 
 app.use(handleError);
+
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 export default app;
